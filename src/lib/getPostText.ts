03ecdf3e3499aadb2efc6bc278d61d6af43f7528
Scott Mendenko <mascott106@gmail.com>
2023-12-07 15:17:21 -0500

    GH Actions Changes
    Testing posting with configured secrets

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -1,4 +1,4 @@
 export default async function getPostText() {
   // Generate the text for your post here. You can return a string or a promise that resolves to a string
-  return "We should now be posting every 22 minutes!";
+  return "Hello! Are you receiving this transmission?";
 }
