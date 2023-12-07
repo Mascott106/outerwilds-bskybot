51a6d6e4795b38fc95e485c93e2d4e440254bd6d
Scott Mendenko <mascott106@gmail.com>
2023-12-07 16:04:57 -0500

    Changed Cron schedule, post quote

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -1,4 +1,4 @@
 export default async function getPostText() {
   // Generate the text for your post here. You can return a string or a promise that resolves to a string
-  return "Hello! Are you receiving this transmission?";
+  return "Something is happening every 22 minutes!";
 }
