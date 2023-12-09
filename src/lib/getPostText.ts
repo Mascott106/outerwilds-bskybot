01e568e3bbdeec6f7f1758d23c52b48cdc445f62
Scott Mendenko <mascott106@gmail.com>
2023-12-08 20:05:12 -0500

    Removed Quotes, Started on Post Logic
    Also fixed typescript config

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -1,4 +1,21 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 export default async function getPostText() {
   // Generate the text for your post here. You can return a string or a promise that resolves to a string
+
+// var gotText = false;
+
+// THIS WILL SELECT THE NUMBER OF THE QUOTE
+
+//   while (gotText == false) {
+//     var selectNum = getRandomInt(101)
+//     gotText = true;
+
+//   }
+
+
+
   return "Something is happening every 22 minutes!";
 }
