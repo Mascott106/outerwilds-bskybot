2a58e934f4953d9be424658785bfcdae00d88529
Scott Mendenko <mascott106@gmail.com>
2023-12-11 19:15:26 -0500

    Working Bot
    Final fixes to lang_en.json removing formatting
    Fixed JSON module in Typescript Config
    Changed to every 22 minutes for the bot in GHA
    
    getPostText now pulls in random number from lang_en.json, checks length, and regenerates if necessary

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -1,3 +1,5 @@
+import lang_en from './lang_en.json' assert { type: 'json' };
+
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
@@ -5,17 +7,24 @@ function getRandomInt(max) {
 export default async function getPostText() {
   // Generate the text for your post here. You can return a string or a promise that resolves to a string
 
-// var gotText = false;
+  var gotText = false;
+  var selectNum;
+  var quoteText;
+  var confirmedQuoteText;
 
-// THIS WILL SELECT THE NUMBER OF THE QUOTE
+  // THIS WILL SELECT THE NUMBER OF THE QUOTE
+  while (!gotText) {
+    selectNum = getRandomInt(2901);
+    quoteText = lang_en[selectNum][0]
 
-//   while (gotText == false) {
-//     var selectNum = getRandomInt(101)
-//     gotText = true;
+    if (quoteText.length < 300) {
+      confirmedQuoteText = quoteText
+      gotText = true;
+    } else {
+        selectNum = getRandomInt(2901);
+        quoteText = lang_en[selectNum][0]
+    }
+  }
 
-//   }
-
-
-
-  return "Something is happening every 22 minutes!";
+  return confirmedQuoteText;
 }
