e6e4289885b27481ab2a467c860e1c9c2f558ca6
Scott Mendenko <mascott106@gmail.com>
2023-12-13 14:24:51 -0500

    Lots of changes
    Removed example environment variables
    
    Edited Package.json data to be me
    
    Made some changes in index.ts for easier testing
    
    Started removing bad quotes from lang_en.json
    Will want to combine some quotes into single string with newline characters
    
    Implemented testing into getPostText.ts
    Now checks for:
    SelectNum existing in database (shouldn't do anything yet, but when I start cleaning up the json it wilL)
    Functionality to test specific quotes
    Logging of tested quotes to console, with verbose feedback

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import Bot from "./lib/bot.js";
 import getPostText from "./lib/getPostText.js";
 
-const text = await Bot.run(getPostText);
+// PROD
+//const text = await Bot.run(getPostText); 
+
+// TESTING
+const text = await Bot.run(getPostText, { dryRun: true });
 
 console.log(`[${new Date().toISOString()}] Posted: "${text}"`);
--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -11,20 +11,42 @@ export default async function getPostText() {
   var selectNum;
   var quoteText;
   var confirmedQuoteText;
+  var testBool = false;
 
   // selects quote number in bounds and checks if it is less than 301 characters
   while (!gotText) {
     selectNum = getRandomInt(2901);
-    quoteText = lang_en[selectNum][0]
 
-    console.log("quoteText");
 
-    if (quoteText.length < 301) {
-      confirmedQuoteText = quoteText       //TODO add quotes around text??? will need to set bounds to 299 just in case
+// USED FOR TESTING NONEXISTENT OBJECTS RETURNED    
+    if (lang_en[selectNum]) {
+      quoteText = lang_en[selectNum][0];
+    } else {
+      console.log(`Key ${selectNum} does not exist in quotes database!`)
+      quoteText = "";
+    }
+
+// USED FOR TESTING SPECIFIC OBJECTS
+         if (!testBool) {
+          quoteText = lang_en[125][0];
+          } else {
+           quoteText = lang_en[selectNum][0];
+       }
+
+    //quoteText = lang_en[selectNum][0];
+
+    console.log(quoteText);
+
+    console.log("Testing quote: \n \"" + quoteText + "\"")
+
+    if (typeof quoteText == 'string' && quoteText.length < 301 && quoteText.length > 1) {
+      confirmedQuoteText = quoteText      //TODO add quotes around text??? will need to set bounds to 299 just in case
       gotText = true;
-      } else {
-        console.log("quote too long");
-      }
+    } else {
+      console.log(quoteText);
+      console.log("Quote outside of boundaries. Trying again.");
+      testBool = true;
+    }
   }
 
   return confirmedQuoteText;
