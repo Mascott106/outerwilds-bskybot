a1b6de7d3dd5ea846fcabdcfe39721e517029b7e
Scott Mendenko <mascott106@gmail.com>
2023-12-14 13:31:33 -0500

    Update index.ts
    Hold up this needs to send to prod

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import Bot from "./lib/bot.js";
 import getPostText from "./lib/getPostText.js";
 
 // PROD
-//const text = await Bot.run(getPostText); 
+const text = await Bot.run(getPostText); 
 
 // TESTING
-const text = await Bot.run(getPostText, { dryRun: true });
+//const text = await Bot.run(getPostText, { dryRun: true });
 
 console.log(`[${new Date().toISOString()}] Posted: "${text}"`);
